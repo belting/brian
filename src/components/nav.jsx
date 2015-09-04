6fe3b6841fa7ba3fb2a853491e6719227bf2e168
Brian Elting <belting01@gmail.com>
2015-09-04 11:18:15 -0700

    Nav menu for small screens

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,7 +5,7 @@ import React from 'react/addons';
 class NavigationList extends React.Component { // eslint-disable-line no-unused-vars
   render() {
     return (
-      <ul>
+      <ul className="nav-menu">
         {this.props.items.map(item => {
           return (
             <li key={item.id}>
@@ -21,7 +21,7 @@ class NavigationList extends React.Component { // eslint-disable-line no-unused-
 class NavigationToggle extends React.Component {render() { // eslint-disable-line no-unused-vars
     return (
       <a className='hidden-md hidden-lg nav-toggle' href="" onClick={this.props.onToggleClick.bind(null)}>
-        <i className="material-icons">&#xE5D2;</i>
+        <i className="material-icons md-36">&#xE5D2;</i>
       </a>
     );
   }
@@ -31,7 +31,8 @@ export class Navigation extends React.Component {constructor(props) {
     super(props);
     this.state = {
       isScrolled: false,
-      isOpen: false
+      isOpen: false,
+      isScreenSmall: false
     };
     this.headerItem = {
       id: 'header'
@@ -40,15 +41,25 @@ export class Navigation extends React.Component {constructor(props) {
   componentDidMount() {
     window.addEventListener('scroll', this.onScroll.bind(this));
     this.onScroll();
+
+    window.addEventListener('resize', this.onResize.bind(this));
+    this.onResize();
   }
   componentWillUnmount() {
     window.removeEventListener('scroll', this.onScroll.bind(this));
+    window.removeEventListener('resize', this.onResize.bind(this));
   }
   getScrollTop() {
     return window.pageYOffset || document.documentElement.scrollTop;
   }
+  getWindowWidth() {
+    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  }
   onItemClick(item, event) {
     event.preventDefault();
+    this.setState({
+      isOpen: false
+    });
     this.scrollTo(document.getElementById(item.id).offsetTop - this.nav.offsetHeight);
   }
   onToggleClick(event) {
@@ -62,24 +73,45 @@ export class Navigation extends React.Component {constructor(props) {
       scrollTop: offsetTop
     }, '500', 'swing');
   }
+  onResize() {
+    const SCREEN_SM_MAX = 991;
+    this.setState({
+      isScreenSmall: this.getWindowWidth() <= SCREEN_SM_MAX
+    });
+    this.onScroll();
+  }
   onScroll() {
     let isScrolled = this.getScrollTop() >= this.header.offsetHeight - this.nav.offsetHeight;
     this.setState({isScrolled});
   }
   render() {
+    let navWrapperClass = 'nav-wrapper';
+    if (this.state.isScrolled) {
+      navWrapperClass += ' scrolled';
+    }
+    if (this.state.isOpen) {
+      navWrapperClass += ' open';
+    }
+
+    let navMenu = <NavigationList items={this.props.items} onItemClick={this.onItemClick.bind(this)}/>;
+    let navMenuFull;
+    let navMenuSmall;
+    if (this.state.isScreenSmall) {
+      navMenuSmall = navMenu;
+    } else {
+      navMenuFull = navMenu;
+    }
+
     return (
-      <div className={'nav-wrapper' + (this.state.isScrolled
-        ? ' scrolled'
-        : '') + (this.state.isOpen
-        ? ' open'
-        : '')}>
+      <div className={navWrapperClass}>
+        {navMenuSmall}
         <div className="container">
           <div className="row">
             <div className="col-md-3">
               <a className="name" href="#header" onClick={this.onItemClick.bind(this, this.headerItem)}>Brian Elting</a>
             </div>
             <div className="col-md-9">
-              <NavigationList items={this.props.items} onItemClick={this.onItemClick.bind(this)}/>
+              {navMenuFull}
             </div>
           </div>
         </div>
