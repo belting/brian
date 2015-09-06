a4e388122210f4fd5361f8c9e6f2e31b71e98c33
Brian Elting <belting01@gmail.com>
2015-09-06 01:27:18 -0700

    Html encoded email address to deter bots

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,25 +1,26 @@
 'use strict';
 
+import he from 'he';
 import mainModel from '../models/main.json';
 import React from 'react/addons';
 import {ContactForm} from '../components/contact';
-import {
-  Navigation
-}
+import {Navigation}
 from '../components/nav';
 
 export default function(req, res) {
-  let contactFormHtml = React.renderToString(
-    React.createFactory(ContactForm)()
-  );
-  let navigationHtml = React.renderToString(
-    React.createFactory(Navigation)({
-      items: mainModel.nav
-    })
-  );
+  let contactFormHtml = React.renderToString(React.createFactory(ContactForm)());
+  let navigationHtml = React.renderToString(React.createFactory(Navigation)({
+    items: mainModel.nav
+  }));
+
+  let heOptions = {
+    encodeEverything: true
+  };
 
   res.render('index', {
     html: {
+      contactEmail: he.encode(mainModel.contact.email, heOptions),
+      contactEmailLink: he.encode(`mailto:${mainModel.contact.email}`, heOptions),
       contactForm: contactFormHtml,
       nav: navigationHtml
     },
