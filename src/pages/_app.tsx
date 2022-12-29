39136ecad3004c1adb051a8fea2eab3860a75c39
Brian Elting <belting01@gmail.com>
2022-12-29 13:03:05 -0800

    refactor: Use @next/font for font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,17 @@ import "normalize.css";
 import "bootstrap/dist/css/bootstrap-grid.css";
 import "../styles/globals.css";
 
+import { Roboto_Flex } from "@next/font/google";
 import type { AppProps } from "next/app";
 
+const robotoFlex = Roboto_Flex({
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={robotoFlex.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500&display=optional"
-          rel="stylesheet"
-          type="text/css"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
